8841561bdc43ca3ba59f69629c547655cccc79ee
karpeykin <karpejkin2016@gmail.com>
2021-02-03 08:24:39 +0700

    Добавил округление до сотых

--- src/main/java/com/example/TestSecurity/converter/Choice.java
+++ src/main/java/com/example/TestSecurity/converter/Choice.java
@@ -5,8 +5,8 @@ import com.example.TestSecurity.model.Valute;
 public class Choice {
 
     public static Double converted(Valute valuteFrom, Valute valuteTo, Integer quantity){
-        return (valuteFrom.getValue() / valuteFrom.getNominal())
-                / (valuteTo.getValue() / valuteTo.getNominal()) * quantity;
+        return Math.rint(((valuteFrom.getValue() / valuteFrom.getNominal())
+                / (valuteTo.getValue() / valuteTo.getNominal()) * quantity) * 100.0) /100.0;
     }
     //Сделать метод конвертации
 }
